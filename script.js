bef10b71a29261d8935722f73bc1790e21a8458a
Awesomegabby <98163532+Awesomegabby@users.noreply.github.com>
2024-02-01 22:00:35 +0100

    Add leaflet library

--- script.js
+++ script.js
@@ -10,3 +10,40 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+const success = function (position) {
+  const { latitude } = position.coords;
+  const { longitude } = position.coords;
+  console.log(latitude, longitude);
+  console.log(
+    `https://www.google.com/maps/@${latitude},${longitude},15z?entry=ttu`
+  );
+
+  const coords = [latitude, longitude];
+
+  const map = L.map('map').setView(coords, 13);
+
+  L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(map);
+
+  L.marker(coords)
+    .addTo(map)
+    .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+    .openPopup();
+};
+
+const error = function () {
+  alert('Could not get your position');
+};
+
+const options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0,
+};
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(success, error, options);
+}
